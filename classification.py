1727870c3b06381f490fa23264f06a5571cdac9d
wangjie <momeijw@gmail.com>
2024-04-01 09:00:12 +0800

    修改测试脚本

--- classification.py
+++ classification.py
@@ -1,20 +1,23 @@
 import ddddocr
+import shutil
 
-det = ddddocr.DdddOcr(show_ad = False, import_onnx_path = './projects/piaoxingqiu/models/effnetv2_m_3/piaoxingqiu_0.96875_1176_65000_2024-03-28-14-29-37.onnx',
+det = ddddocr.DdddOcr(show_ad = False, import_onnx_path = './projects/piaoxingqiu/models/effnetv2_m_3/piaoxingqiu_1.0_3552_159000_2024-04-01-08-46-36.onnx',
                       charsets_path = './projects/piaoxingqiu/models/effnetv2_m_3/charsets.json',)
 
-for i in range(1500, 1510):
-    with open(f'./projects/piaoxingqiu/datasets_o/images/{i}.png', 'rb') as f:
-        image_bytes = f.read()
+# for i in range(1500, 1510):
+#     with open(f'./projects/piaoxingqiu/datasets_o/images/{i}.png', 'rb') as f:
+#         image_bytes = f.read()
 
-    print(det.classification(image_bytes))
+#     print(det.classification(image_bytes))
 
-print('\n\n')
+# print('\n\n')
 
 
-for i in range(1500, 1510):
+for i in range(3600, 3630):
     with open(f'./projects/piaoxingqiu/datasets/images/{i}.png', 'rb') as f:
         image_bytes = f.read()
+        
+    shutil.copy(f'./projects/piaoxingqiu/datasets/images/{i}.png', f'./eval/{i}.png')
 
     print(det.classification(image_bytes))
 
